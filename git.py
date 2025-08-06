15f0ff115507cb456600fbf5480d2d49e5cc7eeb
Ana-Ramos <ana.beatriz055@hotmail.com>
2025-08-06 19:44:43 -0300

    Zuando seu champ no lol

---
+++ git.py
@@ -0,0 +1,8 @@
+seu_main = input("Qual seu main champ no lol?")
+if seu_main == "Draven":
+    print(f"Main {seu_main}, você é tcholas!")
+elif seu_main == "Master yi":
+    print(f"Mono {seu_main}? Você tbm é tcholas amigo.")
+else:
+    ("Pdp cara, considerável.")
+    
