40566c081b3a026561339b8fb19c96e22e1ad3d5
Ana-Ramos <ana.beatriz055@hotmail.com>
2025-08-06 19:49:41 -0300

    Meus slashers favs

--- git.py
+++ git.py
@@ -8,3 +8,11 @@ else:
 
 
 #Olha, tudo contra master yi, ele acaba com o meus adczinho, Draven tenho ranço momentâneo mas vou superar, rs
+
+#Agora depois dessa zuera, diga seus 5 slashers favoritos
+
+slasher1 = "O massacre da serra elétrica"
+slasher2= "A noiva de Chucky"
+slasher3= "Sexta-feira 13"
+slasher4= "Halloween"
+slasher5= "Trick or treat"
